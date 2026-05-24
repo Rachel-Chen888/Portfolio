dda602dd9b905aa8008715f175d443d4060fa94c
unknown <rachel.luo887@gmail.com>
2026-05-24 14:06:35 -0400

    feat: floating navbar and unfinished about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
 
 import './App.css'
-import Button from 'react-bootstrap/Button'
-
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import RachelPic from './assets/RachelPic.png'
+import logo from './assets/logo.png'
 
 export default function App() {
 
   return (
     <>
+    
+      <Navbar expand="lg" className="customNavbar" fixed="top">
+      <Container>
+        <Navbar.Brand href="#about"> 
+          <img
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Rachel Luo Logo" 
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="w-100 justify-content-evenly">
+            <Nav.Link href="#workflow">Workflow</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       <main className="mainContent">
-        
-        <h1 className="portfolioTitle">Rachel Luo Portfolio</h1>
-        <p>Welcome to my portfolio! Here you can find information about my projects, skills, and experience.</p>
-        <button className="btn btn-success">
-          Test Button
-        </button>
-        <Button variant="success">
-          Click Me
-        </Button>
+        <section id="home">
+            <h1 className="portfolioTitle">Hi I'm Rachel Luo</h1>
+            <div className="d-flex align-items-center gap-4">
+              <img
+                src={RachelPic}  
+                width="330"
+                height="330"
+                className="d-inline-block align-top"
+                alt="Rachel Luo Headshot" 
+              />
+            
+              <p>Welcome to my portfolio! Here you can find information about my projects, skills, and experience.</p> 
+            </div>
+        </section>
         <h2>Projects</h2>
         <ul>
             <li><a href="service.html">Project 1: Service Site</a></li>
