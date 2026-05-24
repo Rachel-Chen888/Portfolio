67987d23fc33dfe03bb59c80ee94b7f35c1b5fac
unknown <rachel.luo887@gmail.com>
2026-05-23 22:49:50 -0400

    feat: add react and react-bootstrap

---
+++ Portfolio/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+// From installation: 
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
